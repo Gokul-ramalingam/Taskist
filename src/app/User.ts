8548bfdb38bee2c8aef6e9013bcff2d76fd86c55
Gokul <gokulramalingam1@gmail.com>
2019-10-15 01:31:39 +0530

    implementation of new model

--- src/app/User.ts
+++ src/app/User.ts
@@ -1,7 +1,5 @@
 export class User{
-    constructor(
-        public username:string,
-        public email:string,
-        public password:string
-    ){}
+        public username:string;
+        public email:string;
+        public password:string;
 }
