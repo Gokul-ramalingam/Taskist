4cb45285ee151d2fbd981bba79d1302610cf764d
Gokul <gokulramalingam1@gmail.com>
2019-10-15 03:13:32 +0530

    Update in service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { User } from './User'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class UserService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   selectedUser:User = {
-    username:' ',
-    email:' ',
-    password:' '
+    username:'',
+    email:'',
+    password:''
+  }
+
+  postUser(user:User){
+    return this.http.post(environment.apiBaseUrl+'/register',user);
   }
 
 }
