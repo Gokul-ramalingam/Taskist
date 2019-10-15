ceef0330e11fe8708d266c773c4593c9173ffb51
Gokul <gokulramalingam1@gmail.com>
2019-10-15 11:42:12 +0530

    Update in main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+   toggle:boolean = true;
+  constructor(private router:Router,private routes:ActivatedRoute) { }
 
   ngOnInit() {
+    this.toggle = this.routes.url['value'][0].path === 'main' ? true:false;
   }
 
 }
