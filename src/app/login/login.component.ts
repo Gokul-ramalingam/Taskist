5b8251233ab8350cc145606daf754ff8c13eac10
Gokul <gokulramalingam1@gmail.com>
2019-10-15 11:46:37 +0530

    Addition of email pattern

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,7 @@ import { Router,ActivatedRoute } from '@angular/router'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- 
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   constructor(private userService:UserService,private router:Router,private routes:ActivatedRoute) { }
 
   ngOnInit() {
